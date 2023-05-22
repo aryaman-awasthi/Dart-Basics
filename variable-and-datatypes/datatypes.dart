eb0df447dc6ff565bba32960bfa8650362741ac0
aryaman-awasthi <aryaman.awasthi79@gmail.com>
2023-05-23 02:03:16 +0530

    docs:added basics.txt

---
+++ variable-and-datatypes/datatypes.dart
@@ -0,0 +1,15 @@
+void main() {
+  // PRIMITIVE DATATYPES
+  bool boolVar = true;
+  int intVar = 10;
+  double doubleVar = 4.7;
+  print('PRIMITIVE DATATYPES');
+  print(' boolVar: $boolVar \n intVar: $intVar \n doubleVar: $doubleVar');
+
+  //COMPLEX DATATYPES
+  String strVar = 'text';
+  List lsVar = [1, 2, 3];
+  Map mapVar = {'key': 'val'};
+  print('COMPLEX DATATYPES');
+  print(' strVar: $strVar \n lsVar: $lsVar \n mapVar: $mapVar');
+}
